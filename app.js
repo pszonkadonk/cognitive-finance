f6712217f7c564965932238f0caeb98a282cc8d9
Michael Pszonka <pszonkadonk@Michaels-MacBook-Air.local>
2017-02-25 13:27:18 -0500

    Implement Yahoo Finance RSS api feed

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ var express = require('express');
 
 // Alchemy API and Key
 var AlchemyLanguage = require('watson-developer-cloud/alchemy-language/v1');
-var AlchemyDataNewsV1 = require('watson-developer-cloud/alchemy-data-news/v1');
 var alchemyApiKey = {api_key: process.env.ALCHEMY_API_KEY || 'e159c11d8dda60a89823f4871028767ebecfe68b'}
 var alchemy_language = new AlchemyLanguage(alchemyApiKey)
-var alchemy_data_news = new AlchemyDataNewsV1(alchemyApiKey)
 
 // cfenv provides access to your Cloud Foundry environment
 var cfenv = require('cfenv');
@@ -22,6 +20,8 @@ var hbs = require('hbs');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var request = require('request');
+var Feedparser = require('feedparser');
+
 
 
 // create a new express server
@@ -87,14 +87,8 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 
 
 
-// var params = {
-//   extract: 'entities, keywords',
-//   sentiment: 1, 
-//   maxRetrieve: 1,
-//   url: 'https://www.ibm.com/us-en'
-// };
 
-// alchemy_language.combined(params, function(err, res) {
+// alchemy_language.sentiment(params, function(err, res) {
 //   if(err)
 //     console.log('error', err)
 //   else 
@@ -102,48 +96,47 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 // });
 
 
-// Alchemy Data News Code
 
-// var params = {
-//   start: 'now-1d',
-//   end: 'now',
-//   count: 1,
-//   return: "enriched.url.title",
-// };
+var params = 'http://finance.yahoo.com/rss/headline?s=IBM';
 
+stockReq = request(params);
+var feedparser = new Feedparser([params])
 
-var options = { method: 'GET',
-  url: 'https://access.alchemyapi.com/calls/data/GetNews',
-  qs: 
-   { apikey: 'e159c11d8dda60a89823f4871028767ebecfe68b',
-     outputMode: 'json',
-     start: 'now-1d',
-     end: 'now',
-     return: 'enriched.url.title,enriched.url.text', 
-     count: 1,
-     'q.enriched.url.enrichedTitle.entities.entity': '|text=IBM,type=company|'},
-      headers: { 'content-type': 'application/json' } 
+stockReq.on('error', function (error) {
+  console.log(error)
+});
 
-};
-
-// console.log(options)
-
-request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {  
-    // fullText = body.result.docs[0].source.enriched.url.text;
-    console.log(body);
-    // console.log(fullText)
+stockReq.on('response', function (res) {
+  var stream = this; // `this` is `req`, which is a stream
+  if (res.statusCode !== 200) {
+    this.emit('error', new Error('Bad status code'));
+  }
+  else {
+    stream.pipe(feedparser);
   }
 });
 
+feedparser.on('error', function (error) {
+  console.log(error)
+});
 
+feedparser.on('readable', function () {
+  // This is where the action is!
+  var stream = this; // `this` is `feedparser`, which is a stream
+  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+  var item;
 
-// alchemy_data_news.getNews(params, function(err, res) {
-//   if(err)
-//     console.log(err)
-//   console.log(JSON.stringify(res, null, 2))
-//   fs.writeFile('news.txt', JSON.stringify(res, null, 2), function(err) {
-//     return console.log(err)
-//   });
-// });
-
+  while (item = stream.read()) {
+    params = {
+      url: item.link
+    };
+    // articleLink = item.link.split("*")[1]
+    // console.log(articleLink)
+    alchemy_language.sentiment(params, function(err, res) {
+      if(err)
+        console.log('error', err)
+      else 
+        console.log(JSON.stringify(res, null, 2));
+    });
+  }
+});
