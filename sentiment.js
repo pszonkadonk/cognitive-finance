91adbe274426fcd348503c0ca3c575f49421d10c
Michael Pszonka <pszonkadonk@Michaels-MacBook-Air.local>
2017-03-04 14:27:33 -0500

    allow incrementing and updating of Stock sentiment

--- sentiment.js
+++ sentiment.js
@@ -8,21 +8,37 @@ const Q = require('q');
 const Stock = require('./models/Stock');
 
 function multipleSentiments(articles) {
+    let alchemyArray = []
+    return new Promise((resolve, reject) =>{
+        async.forEach(articles, (link, callback) => {
+            // console.log(link);
+            getSentiment(link);
+            callback();
+        });
+    });
+}
 
-    async.forEach(articles, (link, callback) => {
-        // console.log(link);
-        alchemy_language.sentiment(link, (error, alchemyResponse) => {
-            if(error) {
-                console.log("what i got; " + link.url);
-                console.log(error)
+function getSentiment(linkObject) {
+    return new Promise((resolve, reject) => {
+        alchemy_language.sentiment(linkObject, function(err, alchemyResponse) {
+            if(err) {
+                console.log(err);
             }
             else {
-                console.log(alchemyResponse)
+                Stock.findOneAndUpdate({ticker: 'IBM'},
+                { $inc: {sentiment: alchemyResponse.docSentiment.score} }, {new: true}).exec((err,stock) => {
+                    if(err) {
+                        console.log(err)
+                    }
+                    else {
+                        console.log("Updated stock");
+                        // console.log(JSON.stringify(stock, null, 2));
+                    }
+                });
             }
+            console.log(alchemyResponse);
         });
-        callback();
     });
-    // return(sentiment(param));
 }
 
 
@@ -30,30 +46,29 @@ function multipleSentiments(articles) {
 
 
 
-
-function getSentiment(id) {
-    let params = {
-    url: "https://www.nytimes.com/2017/03/03/opinion/paul-ryans-misguided-sense-of-freedom.html?ref=opinion" 
-    }  
+// function getSentiment(id) {
+//     let params = {
+//     url: "https://www.nytimes.com/2017/03/03/opinion/paul-ryans-misguided-sense-of-freedom.html?ref=opinion" 
+//     }  
  
-     alchemy_language.sentiment(params, function(err, alchemyResponse) {
-        if(err) {
-            console.log(err);
-        }
-        else {
-            Stock.findByIdAndUpdate(id,
-             { sentiment: alchemyResponse.docSentiment.score }, {new: true}).exec((err,stock) => {
-                if(err) {
-                     console.log(err)
-                 }
-                 else {
-                     console.log("Updated stock");
-                     console.log(JSON.stringify(stock, null, 2));
-                 }
-             });
-        }
-    });
-}
+//      alchemy_language.sentiment(params, function(err, alchemyResponse) {
+//         if(err) {
+//             console.log(err);
+//         }
+//         else {
+//             Stock.findByIdAndUpdate(id,
+//              { sentiment: alchemyResponse.docSentiment.score }, {new: true}).exec((err,stock) => {
+//                 if(err) {
+//                      console.log(err)
+//                  }
+//                  else {
+//                      console.log("Updated stock");
+//                      console.log(JSON.stringify(stock, null, 2));
+//                  }
+//              });
+//         }
+//     });
+// }
 
 function resetSentiment(id) {
     Stock.findByIdAndUpdate(id, { sentiment: 0 }, (err, res) => {
